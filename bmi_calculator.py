66e276e3380bed25fd2efd9f084c050c515d3286
Steilblack <spokoinpsich@mail.ru>
2026-04-03 20:35:35 +0700

    Добавил idea в gitignore

---
+++ bmi_calculator.py
@@ -0,0 +1,18 @@
+def calculate_bmi(weight, height):
+    # реализовать расчет ИМТ
+    pass
+def interpret_bmi(bmi):
+    # реализовать интерпретацию ИМТ
+    pass
+def ideal_weight(height, gender):
+    # реализовать расчет идеального веса
+    pass
+def save_result(name, bmi, category):
+    # реализовать сохранение результата
+    pass
+def main():
+    print("Добро пожаловать в BMI Calculator!")
+
+
+if __name__ == "__main__":
+    main()
